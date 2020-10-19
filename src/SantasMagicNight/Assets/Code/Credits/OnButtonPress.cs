7ebe47723cc1317f1419293b2c0df2a0aeb27ce5
SilasReinagel <silas.reinagel@gmail.com>
2020-10-19 16:21:57 -0700

    Scene: Setup skeleton Credits Scene. Closes #11

---
+++ src/SantasMagicNight/Assets/Code/Credits/OnButtonPress.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnButtonPress : MonoBehaviour
+{
+    [SerializeField] private string[] buttonNames;
+    [SerializeField] private UnityEvent action;
+
+    private bool _triggered;
+    
+    void Update()
+    {
+        if (!_triggered && buttonNames.Any(Input.GetButton))
+        {
+            _triggered = true;
+            action.Invoke();
+        }
+    }
+}
