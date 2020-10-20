16325b287849a6bbb7255babd09ed2f4396888c6
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-10-20 16:25:11 -0300

    New Pushing Tile Implemented
    Pushes anything that steps on it to the direction it is pointing to

---
+++ src/SantasMagicNight/Assets/Scenes/Dan/OnPushingTileStep.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class OnPushingTileStep : OnMessage<PieceMoved>
+{
+    [SerializeField] Vector2 direction = Vector2.up;
+
+    protected override void Execute(PieceMoved msg)
+    {
+        if (!msg.HasSelected(gameObject)) return;
+        Debug.Log("Stepped On: " + gameObject.name);
+        Message.Publish(new PieceMoved(msg.Piece, msg.To, msg.To + new TilePoint(direction)));
+    }
+}
