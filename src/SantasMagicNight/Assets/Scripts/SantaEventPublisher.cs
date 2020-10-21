ee3a8a96b92698792a9c3bd1ab1435ca113611b0
SilasReinagel <silas.reinagel@gmail.com>
2020-10-20 22:48:55 -0700

    UI: Updated Level Complete Visuals. Closes #74

--- src/SantasMagicNight/Assets/Scripts/SantaEventPublisher.cs
+++ src/SantasMagicNight/Assets/Scripts/SantaEventPublisher.cs
@@ -7,4 +7,6 @@ public class SantaEventPublisher : ScriptableObject
     public void ShowOptionsMenu() => Message.Publish(new ShowOptionsMenu());
     public void ShowLevelSelect() => Message.Publish(new ShowLevelSelect());
     public void HideCurrentSubMenu() => Message.Publish(new HideCurrentSubMenu());
+    public void GoToNextLevel() => Message.Publish(new GoToNextLevel());
+    public void RetryLevel() => Message.Publish(new RetryLevel());
 }
