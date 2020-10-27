51b784771fdd9568e7d50e8301c8f435ba11b7f7
SilasReinagel <silas.reinagel@gmail.com>
2020-10-26 19:09:06 -0700

    UI: Added Pagination to Level Select. Wired in Zone 2 levels. Closes #70

--- src/SantasMagicNight/Assets/Scripts/UI/SantaLevelSelect/InitSantaLevelSelectButtons.cs
+++ src/SantasMagicNight/Assets/Scripts/UI/SantaLevelSelect/InitSantaLevelSelectButtons.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class InitSantaLevelSelectButtons : MonoBehaviour
 {
@@ -8,26 +10,56 @@ public class InitSantaLevelSelectButtons : MonoBehaviour
     [SerializeField] private TextCommandButton buttonPrototype;
     [SerializeField] private CurrentZone currentZone;
     [SerializeField] private GameObject parent;
+    [SerializeField] private Button previousButton;
+    [SerializeField] private Button nextButton;
 
     private int _zone = 0;
     
     void Awake()
     {
-        foreach(Transform t in parent.transform)
-            Destroy(t.gameObject);
-        
         if (currentZone.Campaign != campaign)
             currentZone.Init(campaign);
+        
+        previousButton.onClick.AddListener(MovePrevious);
+        nextButton.onClick.AddListener(MoveNext);
+        UpdateUi();
+    }
+
+    private void UpdateUi()
+    {
+        foreach(Transform t in parent.transform)
+            Destroy(t.gameObject);
         
         var zone = campaign.Value[_zone];
         for (var i = 0; i < zone.Value.Length; i++)
         {
             var currentIndex = i;
-            Instantiate(buttonPrototype, parent.transform).Init($"Level {i + 1}", () => 
+            Instantiate(buttonPrototype, parent.transform).Init($"{_zone + 1} - {i + 1}", () => 
             { 
                 level.SelectLevel(zone.Value[currentIndex], 0, currentIndex);
                 navigator.NavigateToGameScene();
             });
         }
+
+        previousButton.gameObject.SetActive(_zone > 0);
+        nextButton.gameObject.SetActive(_zone < campaign.Value.Length - 1);
+    }
+    
+    private void MovePrevious()
+    {
+        if (_zone == 0)
+            return;
+
+        _zone = Math.Max(0, _zone - 1);
+        UpdateUi();
+    }
+    
+    private void MoveNext()
+    {
+        if (_zone == campaign.Value.Length - 1)
+            return;
+
+        _zone = Math.Min(campaign.Value.Length - 1, _zone + 1);
+        UpdateUi();
     }
 }
