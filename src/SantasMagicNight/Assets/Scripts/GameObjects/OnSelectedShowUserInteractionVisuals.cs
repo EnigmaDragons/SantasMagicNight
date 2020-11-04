236de094fcd8aa19f48179810c5b864f23653b1a
Silas Reinagel <silas.reinagel@carvana.com>
2020-11-04 14:30:20 -0700

    UI: Smaller Happiness UI icon. Closes #114

--- src/SantasMagicNight/Assets/Scripts/GameObjects/OnSelectedShowUserInteractionVisuals.cs
+++ src/SantasMagicNight/Assets/Scripts/GameObjects/OnSelectedShowUserInteractionVisuals.cs
@@ -1,6 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using UnityEngine;
 
+[Obsolete("Old BitVault approach to Hints")]
 public class OnSelectedShowUserInteractionVisuals : MonoBehaviour
 {
     [SerializeField] private GameObject[] options;
