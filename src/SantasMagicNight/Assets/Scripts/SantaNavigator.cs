08b4a94f3c4e61aed69f4ed07238ed7c3606d258
SilasReinagel <silas.reinagel@gmail.com>
2020-11-11 22:06:45 -0700

    UI: Wired up Quit button. Closes #172

--- src/SantasMagicNight/Assets/Scripts/SantaNavigator.cs
+++ src/SantasMagicNight/Assets/Scripts/SantaNavigator.cs
@@ -1,5 +1,4 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 [CreateAssetMenu(menuName = "Santa/Navigator")]
@@ -8,4 +7,13 @@ public class SantaNavigator : ScriptableObject
     public void NavigateToGameScene() => SceneManager.LoadScene("GameScene");
     public void NavigateToCredits() => SceneManager.LoadScene("CreditsScene");
     public void NavigateToMainMenu() => SceneManager.LoadScene("MainMenu");
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
 }
