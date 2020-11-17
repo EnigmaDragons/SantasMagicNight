253c6e308115591f608fde986b81b4094a5880d4
SilasReinagel <silas.reinagel@gmail.com>
2020-11-17 15:49:28 -0700

    Audio: Options Menu always plays sound when dismissed. Closes #203

---
+++ src/SantasMagicNight/Assets/Code/Audio/PlayOnEnableDisableUiSound.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class PlayOnEnableDisableUiSound : MonoBehaviour
+{
+    [SerializeField] private UiSfxPlayer player;
+    [SerializeField] private AudioClipWithVolume onEnableSound;
+    [SerializeField] private AudioClipWithVolume onDisableSound;
+
+    private void OnEnable()
+    {
+        if (onEnableSound != null)
+            player.Play(onEnableSound);
+    }
+
+    private void OnDisable()
+    {
+        if (onDisableSound != null)
+            player.Play(onDisableSound);
+    }
+}
---
+++ src/SantasMagicNight/Assets/Code/ExecuteOnDisable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ExecuteOnDisable : MonoBehaviour
+{
+    [SerializeField] private UnityEvent action;
+
+    private void OnDisable() => action.Invoke();
+}
