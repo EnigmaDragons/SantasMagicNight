792166ec8fdff860f55764c9787d743114d1ab12
SilasReinagel <silas.reinagel@gmail.com>
2020-11-19 13:22:54 -0700

    UI: Added Level Select to Game Scene. Closes #159

--- src/SantasMagicNight/Assets/Scripts/UI/SantaInGameUiController.cs
+++ src/SantasMagicNight/Assets/Scripts/UI/SantaInGameUiController.cs
@@ -1,29 +1,44 @@
-﻿
-using UnityEngine;
+﻿using UnityEngine;
 
-public class SantaInGameUiController : OnMessage<ShowOptionsMenu, HideCurrentSubMenu>
+public class SantaInGameUiController : OnMessage<ShowOptionsMenu, ShowLevelSelect, HideCurrentSubMenu>
 {
     [SerializeField] private GameObject optionsMenu;
     [SerializeField] private GameObject inGameControls;
+    [SerializeField] private GameObject levelSelect;
 
     private void Awake()
     {
         Time.timeScale = 1;
         inGameControls.SetActive(true);
         optionsMenu.SetActive(false);
+        levelSelect.SetActive(false);
     }
     
     protected override void Execute(ShowOptionsMenu msg)
     {
-        inGameControls.SetActive(false);
+        HideAll();
         Time.timeScale = 0;
         optionsMenu.SetActive(true);
     }
 
+    protected override void Execute(ShowLevelSelect msg)
+    {
+        HideAll();
+        Time.timeScale = 0;
+        levelSelect.SetActive(true);
+    }
+
     protected override void Execute(HideCurrentSubMenu msg)
     {
-        optionsMenu.SetActive(false);
+        HideAll();
         Time.timeScale = 1;
         inGameControls.SetActive(true);
+    }
+
+    private void HideAll()
+    {
+        optionsMenu.SetActive(false);
+        inGameControls.SetActive(false);
+        levelSelect.SetActive(false);
     }
 }
